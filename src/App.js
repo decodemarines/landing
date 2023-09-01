d03386406f466a8d65daf60f0c9bad28116b311a
Decode Marines <goipukraine@gmail.com>
2023-09-01 11:36:18 +0100

    basic animation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
+import React from 'react';
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef} from 'react'
+
+function Box() { 
+	const ref = useRef()
+	useFrame(() => { 
+		ref.current.position.x += 0.02
+		ref.current.rotation.y += 0.02
+	})
+
+	return (
+		<mesh ref={ ref}>
+			<boxGeometry />
+	 		<meshBasicMaterial color='aqua' /> 
+	   </mesh> 
+    )
+}
 export default function App() {
-  return 'Hell';
+	// return "hello"
+	 return (
+	 	<Canvas>
+           < Box />
+	    </Canvas>
+	 )
 }
