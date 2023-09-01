212da7307e5311633ddf52d79c9f9a00568122ab
Decode Marines <goipukraine@gmail.com>
2023-09-01 12:53:44 +0100

    z for depth of objects

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { useRef ,useState} from 'react'
 import * as THREE from 'three'
 
-function Box() { 
+function Box({ z}) { 
 	const ref = useRef()
 	const { viewport} = useThree()
 	const [data] = useState({
 		// x: 0,
-		x: THREE.MathUtils.randFloatSpread(viewport.width), // 6 :from -3 to 3
-		y: 0,
+		x: THREE.MathUtils.randFloatSpread(2), // 6 :from -3 to 3
+		y: THREE.MathUtils.randFloatSpread(viewport.height),
 	})
 
 	useFrame((state) => { 
-		ref.current.position.set(0, (data.y += 0.3), 0)
+		ref.current.position.set(data.x * viewport.width, (data.y += 0.1), z)
 		
 		if (data.y > viewport.height / 1.5) { // falls back when reaches 2 to -2
 			data.y = -viewport.height / 1.5
@@ -27,13 +27,11 @@ function Box() {
 	   </mesh> 
     )
 }
-export default function App() {
+export default function App({ count = 50}) {
 	// return "hello"
 	 return (
 	 	<Canvas>
-			 < Box />
-			 < Box />
-			 < Box />
+			 {Array.from({ length: count }, (_, i) => (<Box key={i} z={ -i}/>))}
 			
 	    </Canvas>
 	 )
